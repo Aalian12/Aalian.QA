f5c78caf6aa120958eea09eb11b376dd5acb4938
Aalian12 <146876099+Aalian12@users.noreply.github.com>
2025-06-10 18:47:01 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aalian.QA/', // This matches your repository name exactly
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: './',
 });
